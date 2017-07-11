9dc977aadefb6943ff48c1fc698133ab05d9b30c
drid <drid@stadar.org>
2017-07-11 04:27:04 +0300

    Command line input
    TLE check and CRC check

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TLE2Keplerian
+
+Converts TLE to Keplerian coordinates.  
+Generates GMAT Spacecraft code
+
+## Installation
+pip install pyastro
--- TLE2Keplerian.py
+++ TLE2Keplerian.py
@@ -1,23 +1,7 @@
 from math import pi
 import pyastro
 from datetime import datetime, timedelta
-
-# def MA2TA (MA,Ecc):
-	
-#  	EA = MA = radians(MA)
-
-#  	for cntr in range(0,6):
-#  		a = EA-(Ecc * sin(EA)) - MA
-#  		b = 1-(Ecc * cos(EA))
-#  		EA = EA - (a/b)
-#    	a = cos(EA) - Ecc
-#    	b = 1 - (Ecc * cos(EA))
-#    	print a/b
-#    	TA = acos(a / b)
-
-#  	if TA > pi:
-#  		TA = pi + (pi - TA)
-# 	return degrees(TA)
+from sys import stdin
 
 def MM2SMA(MM):
 	MU = 398600.4418
@@ -25,15 +9,27 @@ def MM2SMA(MM):
 	MM = MM*((2*pi)/86400)
 	return (MU/(MM**2))**(1.0/3.0)
 
+def checksum(line):
+    """Compute the TLE checksum."""
+    return (sum((int(c) if c.isdigit() else c == '-') for c in line[0:-1]) % 10) == int(line[-1]) 
 
-TLE = [
-	"1 42716U 98067LX  17190.68440267  .00012619  00000-0  18341-3 0  9994",
-	"2 42716  51.6404 285.6070 0005210  46.3490 313.7933 15.56166765  8144"
-	]
+print "Enter TLE including object title:"
 
-(line,SAT,Desgnator,TLEEpoch,MM1,MM2,BSTAR,EType,Elementnum) = TLE[0].split()
-(line,SATNum,Inc,RAAN,Ecc,AoP,MA,MM,RevNum) = TLE[1].split()
+TLE=[]
+TLE.append(stdin.readline().strip())
+TLE.append(stdin.readline().strip())
+TLE.append(stdin.readline().strip())
 
+if TLE[1][:2] != '1 ' or checksum(TLE[1]) == False:
+	print "Not a TLE"
+	exit()
+if TLE[2][:2] != '2 'or checksum(TLE[2]) == False:
+	print "Not a TLE"
+	exit()
+
+SatName = TLE[0]
+(line,SAT,Desgnator,TLEEpoch,MM1,MM2,BSTAR,EType,ElementNum) = TLE[1].split()
+(line,SATNum,Inc,RAAN,Ecc,AoP,MA,MM) = TLE[2].split()[:8]
 EpochY = int(TLEEpoch[:2])
 if EpochY > 56:
 	EpochY+=1900
@@ -43,24 +39,23 @@ EpochD = float(TLEEpoch[2:])
 MA = float(MA)
 MM = float(MM)
 Ecc = float('0.'+Ecc)
-# EpochY, EpochD, Inc, RAAN, ECC, AoP, MA, MM, TA, SMA, MJD, TAfromMA, SMAfromMM, MJDfromEpoch
+SMA = MM2SMA(MM)
+TA = pyastro.kepler(MA,Ecc)
 
-print "Year",EpochY,"\nday",EpochD,"\nInc",Inc,"\nRAAN",RAAN,"\nEcc",Ecc,"\nAoP",AoP,"\n-MA",MA,"\n-MM",MM
-
-# print "TA:", MA2TA(MA,Ecc)
-print "TA:", pyastro.kepler(MA,Ecc)
-print "SMA:", MM2SMA(MM)
+print "Year:",EpochY,"\nDay:",EpochD,"\nInc:",Inc,"\nRAAN:",RAAN,
+"\nEcc:",Ecc,"\nAoP:",AoP,"\nMA:",MA,"\nMM:",MM, "TA:", TA, "SMA:", SMA
 Epoch = (datetime(EpochY-1,12,31) + timedelta(EpochD)).strftime("%d %b %Y %H:%M:%S.%f")[:-3]
 print "Epoch:", Epoch
 
-print("\nCreate Spacecraft TLE_SC;\n" +
-		"GMAT TLE_SC.DateFormat = UTCGregorian;\n" +
-		"GMAT TLE_SC.Epoch = '"+Epoch+"';\n" +
-		"GMAT TLE_SC.CoordinateSystem = EarthMJ2000Eq;\n" +
-		"GMAT TLE_SC.DisplayStateType = Keplerian;\n" +
-		"GMAT TLE_SC.SMA = 6821;\n" +
-		"GMAT TLE_SC.ECC = 0.000140300000001036;\n" +
-		"GMAT TLE_SC.INC = 98.7;\n" +
-		"GMAT TLE_SC.RAAN = " + str(RAAN) + ";\n" +
-		"GMAT TLE_SC.AOP = " + str(AoP) + ";\n" +
-		"GMAT TLE_SC.TA = 259.0715000000072;\n")
+print("\nCreate Spacecraft "+SatName+";\n" +
+		"GMAT "+SatName+".Id = '"+SATNum+"';\n" +
+		"GMAT "+SatName+".DateFormat = UTCGregorian;\n" +
+		"GMAT "+SatName+".Epoch = '"+Epoch+"';\n" +
+		"GMAT "+SatName+".CoordinateSystem = EarthMJ2000Eq;\n" +
+		"GMAT "+SatName+".DisplayStateType = Keplerian;\n" +
+		"GMAT "+SatName+".SMA = "+str(SMA)+";\n" +
+		"GMAT "+SatName+".ECC = "+str(Ecc)+";\n" +
+		"GMAT "+SatName+".INC = "+str(Inc)+";\n" +
+		"GMAT "+SatName+".RAAN = " + str(RAAN) + ";\n" +
+		"GMAT "+SatName+".AOP = " + str(AoP) + ";\n" +
+		"GMAT "+SatName+".TA = "+str(TA)+";\n")
